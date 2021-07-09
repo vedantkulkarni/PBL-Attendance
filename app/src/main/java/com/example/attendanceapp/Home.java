ee42b3223459aeee9d49df13b871ee78b6ab1e24
Vedant Kulkarni <vedantk60@gmail.com>
2021-07-09 13:37:35 +0530

    7/9/2021 , worked on back end . App is in most updated state .

---
+++ app/src/main/java/com/example/attendanceapp/Home.java
@@ -0,0 +1,55 @@
+package com.example.attendanceapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class Home extends AppCompatActivity {
+    private FirebaseDatabase aDatabase = FirebaseDatabase.getInstance( "https://attendanceapp-7ed22-default-rtdb.asia-southeast1.firebasedatabase.app/");
+    private Button attendanceBt ;
+    private TextView userattendance;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        userattendance=findViewById(R.id.attendance);
+        attendanceBt=findViewById(R.id.button3);
+        attendanceBt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                aDatabase=FirebaseDatabase.getInstance( "https://attendanceapp-7ed22-default-rtdb.asia-southeast1.firebasedatabase.app/");
+                String currentuser = FirebaseAuth.getInstance().getCurrentUser().getUid();
+//               aDatabase=FirebaseDatabase.getInstance().getReference();
+                aDatabase.getReference().child("Users").child(currentuser).child("username").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DataSnapshot> task) {
+                        if (!task.isSuccessful()) {
+                            Log.e("firebase", "Error getting data", task.getException());
+                            Toast.makeText(Home.this , "failed to get value " , Toast.LENGTH_SHORT).show();
+                        }
+                        else {
+                            Log.d("firebase", String.valueOf(task.getResult().getValue()));
+                            userattendance.setText(String.valueOf(task.getResult().getValue()));
+
+                        }
+                    }
+                });
+            }
+        });
+
+
+    }
+}
--- app/src/main/java/com/example/attendanceapp/SignIn_Activity.java
+++ app/src/main/java/com/example/attendanceapp/SignIn_Activity.java
@@ -3,6 +3,7 @@ package com.example.attendanceapp;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -38,6 +39,8 @@ public class SignIn_Activity extends AppCompatActivity {
                         if(task.isSuccessful())
                         {
                             Toast.makeText(SignIn_Activity.this, "SignIn Successful", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(SignIn_Activity.this , Home.class);
+                            startActivity(intent);
                         }
                         else
                         {
