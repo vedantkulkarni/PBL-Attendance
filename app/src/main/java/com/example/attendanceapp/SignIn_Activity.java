0812068a44c41754bb495ed1811b471bd7fd5a11
omkar kalamkar <omkarkalamkar2003@gmail.com>
2021-07-13 16:11:51 +0530

    icon added on activity_sign_in_.xml page

--- app/src/main/java/com/example/attendanceapp/SignIn_Activity.java
+++ app/src/main/java/com/example/attendanceapp/SignIn_Activity.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
